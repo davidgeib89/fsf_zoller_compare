bce130d14891813f5e7128f354067cce23652b2f
David Geib <d.geib@freesixtyfive.de>
2021-01-25 10:53:48 +0100

    headline

--- Resources/Public/js/main.js
+++ Resources/Public/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-  $("html").on("mouseup", function (e) {
+  /*$("html").on("mouseup", function (e) {
     var l = $(e.target);
     if (l[0].className.indexOf("popover") == -1) {
       $(".popover").each(function () {
         $(this).popover("hide");
       });
     }
-  });
+  });*/
 });
